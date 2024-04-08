835929570fe72faa37129256b43fbd6f09fdd50c
coldsofttech <161084795+coldsofttech@users.noreply.github.com>
2024-04-08 20:39:40 +0100

    Introduced Linting (#3)

--- pyconfigurationmanager/__init__.py
+++ pyconfigurationmanager/__init__.py
@@ -28,11 +28,11 @@ __all__ = [
 ]
 __author__ = "coldsofttech"
 __description__ = """
-The pyconfigurationmanager package provides a set of utilities for managing configuration settings in 
-Python applications. It includes classes and functions for loading configuration data from files, 
+The pyconfigurationmanager package provides a set of utilities for managing configuration settings in
+Python applications. It includes classes and functions for loading configuration data from files,
 auditing configuration changes, and ensuring secure access to configuration files.
 """
 __name__ = "pyconfigurationmanager"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 from pyconfigurationmanager.__main__ import ConfigurationManager, ConfigurationManagerError
